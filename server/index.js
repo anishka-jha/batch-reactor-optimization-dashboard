2f3b4626022d1cd718553f24f388fca86d674076
anishka-jha <anishkajha0508@gmailcom>
2026-06-26 21:50:47 +0530

    Use Render PORT

--- server/index.js
+++ server/index.js
@@ -142,9 +142,11 @@ async function startServer() {
     console.log("MongoDB Connected");
     console.log("Ready State:", mongoose.connection.readyState);
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
+    const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   } catch (err) {
     console.log("MongoDB Connection Failed:", err);
   }
